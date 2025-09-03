c32edac533393a833fa0fdd95180bbc4a0b26d2e
AshishPython11 <ashishpython11@gmail.com>
2025-09-03 23:08:24 +0530

    First prod push

--- O2_bottle_MGMT/settings.py
+++ O2_bottle_MGMT/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'O2_bottle_MGMT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rainbowgases$default',
+        'USER': 'rainbowgases',
+        'PASSWORD': 'Rainbow@1234',
+        'HOST': 'rainbowgases.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
